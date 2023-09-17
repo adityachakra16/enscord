a46f4a02cdf598ab95ed58b833a89e83db4d704f
adityachakra16 <adityachakra16@gmail.com>
2023-09-17 19:14:06 +0530

    fix build issues

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
-import { Client, GatewayIntentBits, Collection, PermissionFlagsBits,} from "discord.js";
-const { Guilds, MessageContent, GuildMessages, GuildMembers } = GatewayIntentBits
-const client = new Client({intents:[Guilds, MessageContent, GuildMessages, GuildMembers]})
+import {
+  Client,
+  GatewayIntentBits,
+  Collection,
+  PermissionFlagsBits,
+  Partials,
+} from "discord.js";
+const { Guilds, MessageContent, GuildMessages, GuildMembers } =
+  GatewayIntentBits;
+const { Channel, GuildMember, User } = Partials;
+const client = new Client({
+  intents: [Guilds, MessageContent, GuildMessages, GuildMembers],
+  partials: [Channel, GuildMember, User],
+});
 import { Command, SlashCommand } from "./types";
 import { config } from "dotenv";
 import { readdirSync } from "fs";
 import { join } from "path";
-config()
+config();
 
-client.slashCommands = new Collection<string, SlashCommand>()
-client.commands = new Collection<string, Command>()
-client.cooldowns = new Collection<string, number>()
+client.slashCommands = new Collection<string, SlashCommand>();
+client.commands = new Collection<string, Command>();
+client.cooldowns = new Collection<string, number>();
 
-const handlersDir = join(__dirname, "./handlers")
-readdirSync(handlersDir).forEach(handler => {
-    if (!handler.endsWith(".js")) return;
-    require(`${handlersDir}/${handler}`)(client)
-})
+const handlersDir = join(__dirname, "./handlers");
+readdirSync(handlersDir).forEach((handler) => {
+  if (!handler.endsWith(".js")) return;
+  require(`${handlersDir}/${handler}`)(client);
+});
 
-client.login(process.env.TOKEN)
+console.log("client");
+client.login(process.env.TOKEN);
