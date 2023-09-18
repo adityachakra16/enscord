ed987ba556bc482fff1559d5a445d26daf57d18b
adityachakra16 <adityachakra16@gmail.com>
2023-09-18 23:53:32 +0530

    show stats on slash command

---
+++ src/slashCommands/stats.ts
@@ -0,0 +1,54 @@
+import {
+  SlashCommandBuilder,
+  ChannelType,
+  TextChannel,
+  EmbedBuilder,
+  Guild,
+} from "discord.js";
+import { getThemeColor } from "../functions";
+import { SlashCommand } from "../types";
+import { bulkVerifyEns } from "../services/bulkVerification";
+
+const command: SlashCommand = {
+  command: new SlashCommandBuilder()
+    .setName("stats")
+    .setDescription(
+      "Shows the number of verified ens names and the number of ens names that are not verified"
+    ),
+  execute: async (interaction) => {
+    const {
+      membersWithEnsNickname,
+      unverifiedEnsMembersWithoutConnectedAccounts,
+      usersWithInValidEnsNames,
+    } = await bulkVerifyEns(interaction.guild as Guild, false);
+    console.log({
+      membersWithEnsNickname,
+      unverifiedEnsMembersWithoutConnectedAccounts,
+      usersWithInValidEnsNames,
+    });
+    interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle("ENS Stats")
+          .setFields([
+            {
+              name: "Members with ENS names",
+              value: membersWithEnsNickname.toString(),
+            },
+            {
+              name: "Members with ENS names that are not verified",
+              value: unverifiedEnsMembersWithoutConnectedAccounts.toString(),
+            },
+            {
+              name: "Members with ENS names that are not owned by them",
+              value: usersWithInValidEnsNames.toString(),
+            },
+          ])
+          .setColor(getThemeColor("text")),
+      ],
+    });
+  },
+  cooldown: 10,
+};
+
+export default command;
