47856e49a5257cb99c598e6b5ee75e7832e643ac
adityachakra16 <adityachakra16@gmail.com>
2023-09-19 14:35:54 +0530

    handle removal of bot from server

---
+++ src/events/guildMemberRemove.ts
@@ -0,0 +1,15 @@
+import { Guild } from "discord.js";
+import GuildModel from "../schemas/Guild";
+import { BotEvent } from "../types";
+
+const event: BotEvent = {
+  name: "guildDelete",
+  execute: async (guild: Guild) => {
+    console.log("guildDelete");
+    try {
+      await GuildModel.deleteOne({ guildID: guild.id });
+    } catch (err) {
+      console.log({ err });
+    }
+  },
+};
