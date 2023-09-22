bf79be0c0eebd971e2396dba80d96520d718d2d3
adityachakra16 <adityachakra16@gmail.com>
2023-09-22 15:14:34 +0530

    fix cors (hopefully for the last time)

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,12 @@ import auth from "./controllers/auth";
 
 console.log(process.env.DISCORD_REDIRECT_URI);
 const corsOptions = {
-  origin: process.env.DISCORD_REDIRECT_URI,
+  origin: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use((req: any, res: any, next: any) => {
-  res.header("Access-Control-Allow-Origin", process.env.DISCORD_REDIRECT_URI);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 app.use(express.static(path.join(__dirname, "frontend/build")));
 app.use("/auth", auth);
 
