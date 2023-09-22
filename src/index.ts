bc38311a373c6cfa513bfd505dc017b706dac102
adityachakra16 <adityachakra16@gmail.com>
2023-09-22 14:53:46 +0530

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,21 @@ const app = express();
 const PORT = 3001;
 import auth from "./controllers/auth";
 
+console.log(process.env.DISCORD_REDIRECT_URI);
 const corsOptions = {
   origin: process.env.DISCORD_REDIRECT_URI,
   credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-
+app.use((req: any, res: any, next: any) => {
+  res.header("Access-Control-Allow-Origin", process.env.DISCORD_REDIRECT_URI);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.static(path.join(__dirname, "frontend/build")));
 app.use("/auth", auth);
 
